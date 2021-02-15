ad1ef06bcbda2298c28fdecc7022294471e7cc8e
Emmanuele Bassi <ebassi@gnome.org>
2021-02-15 01:27:41 +0000

    utils: Use the proper name to find the type
    We just need the relative type name, not the fully qualified one.

--- gidocgen/utils.py
+++ gidocgen/utils.py
@@ -147,7 +147,7 @@ class LinkGenerator:
         elif self._fragment in ['alias', 'class', 'const', 'enum', 'error', 'flags', 'iface', 'struct']:
             self._name = self._rest
             if self._namespace is not None:
-                t = self._namespace.find_real_type(f"{self._ns}.{self._name}")
+                t = self._namespace.find_real_type(self._name)
                 if t is not None:
                     self._type = t.ctype
                 else:
@@ -167,7 +167,7 @@ class LinkGenerator:
         elif self._fragment == 'signal':
             self._name, self._signal_name = self._rest.split('::')
             if self._namespace is not None:
-                t = self._namespace.find_real_type(f"{self._ns}.{self._name}")
+                t = self._namespace.find_real_type(self._name)
                 if t is not None:
                     self._type = t.ctype
                 else:
@@ -177,7 +177,7 @@ class LinkGenerator:
         elif self._fragment in ['ctor', 'method']:
             self._name, self._func_name = self._rest.split('.')
             if self._namespace is not None:
-                t = self._namespace.find_real_type(f"{self._ns}.{self._name}")
+                t = self._namespace.find_real_type(self._name)
                 if t is not None:
                     self._func = f"{self._namespace.symbol_prefix[0]}_{t.symbol_prefix}_{self._func_name}()"
                 else:
