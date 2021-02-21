cef89de4f6c321330893a2168ad652e940401a22
Gil Forcada <gil.gnome@gmail.com>
2021-02-22 00:32:16 +0100

    chore(setup): remove duplicated info
    The same information is found in both `setup.py` and `setup.cfg`, favor
    the one in `setup.cfg`.

--- setup.py
+++ setup.py
@@ -37,18 +37,11 @@ package_data = {
 
 data_files = []
 
-requirements = []
-
 if __name__ == '__main__':
     setup(
         name='gi-docgen',
         version=version,
-        url='https://gitlab.gnome.org/ebassi/gi-docgen',
         license='GPL-3.0-or-later AND Apache-2.0 AND CC0-1.0',
-        author='Emmanuele Bassi',
-        author_email='ebassi@gnome.org',
-        description='gi-docgen is a tool to generate documentation for '
-        'GObject-based libraries',
         long_description=readme_md(),
         long_description_content_type='text/markdown',
         include_package_data=True,
@@ -56,5 +49,4 @@ if __name__ == '__main__':
         package_data=package_data,
         data_files=data_files,
         entry_points=entries,
-        install_requires=requirements,
     )
