f891cc5fd481bc4180eec144d14f32c15c91833b
Emmanuele Bassi <ebassi@gnome.org>
2021-03-15 11:03:33 +0000

    generate: Add link attribute to type subsections
    DevHelp expects all `sub` elements to have a `link` attribute. We use
    named sections for the type indices, to give structure to the reference,
    so we just re-link the index.

--- gidocgen/gdgenerate.py
+++ gidocgen/gdgenerate.py
@@ -2283,6 +2283,7 @@ def gen_devhelp(config, repository, namespace, symbols, content_files):
 
         sub = etree.SubElement(chapters, "sub")
         sub.set("name", section.replace("_", " ").capitalize())
+        sub.set("link", "index.html")
 
         for t in types:
             sub_section = etree.SubElement(sub, "sub")
