f695013c77bdc6fb71969abfd66696153cbc6836
Emmanuele Bassi <ebassi@gnome.org>
2021-03-22 12:03:28 +0000

    Add pkg-config file for gi-docgen
    Sadly, a lowest common denominator solution.
    
    We need to override the build command for setuptools to generate a
    pkg-config file from a template.
    
    To avoid having different results with the Meson build, we cannot use
    the pkgconfig.generate() function, and instead rely on configure_file().
    
    Such is the life of maintainers of projects using multiple build
    systems.
    
    Fixes: #60

--- setup.py
+++ setup.py
@@ -7,7 +7,25 @@ if sys.version_info < (3, 6, 0):
     raise SystemExit('ERROR: GI-DocGen requires Python 3.6.0')
 
 from gidocgen.core import version
+
+from distutils.command.build_py import build_py as _build_py
 from setuptools import setup
+
+
+class BuildCommand(_build_py):
+
+    def generate_pkgconfig_file(self):
+        lines = []
+        with open('gi-docgen.pc.in', 'r') as f:
+            for line in f.readlines():
+                new_line = line.strip().replace('@VERSION@', version)
+                lines.append(new_line)
+        with open('gi-docgen.pc', 'w') as f:
+            f.write('\n'.join(lines))
+
+    def run(self):
+        self.generate_pkgconfig_file()
+        return super().run()
 
 
 def readme_md():
@@ -35,10 +53,11 @@ package_data = {
     ],
 }
 
-data_files = []
-
 if __name__ == '__main__':
     setup(
+        cmdclass={
+            'build_py': BuildCommand,
+        },
         name='gi-docgen',
         version=version,
         license='GPL-3.0-or-later AND Apache-2.0 AND CC0-1.0',
@@ -47,6 +66,6 @@ if __name__ == '__main__':
         include_package_data=True,
         packages=packages,
         package_data=package_data,
-        data_files=data_files,
         entry_points=entries,
+        data_files=[('share/pkgconfig', ['gi-docgen.pc'])],
     )
