94c2457c2d44edb0e6282d372310fb8e684533ea
Bilal Elmoussaoui <belmouss@redhat.com>
2024-01-27 13:46:14 +0100

    types: Don't generate classes hierarchy if not needed
    In case the library doesn't have any gobjects, there would be no classes
    hierarchy to render. In such case, go ahead and even hide the additional documents
    section

--- gidocgen/gdgenerate.py
+++ gidocgen/gdgenerate.py
@@ -2811,6 +2811,9 @@ def gen_types_hierarchy(config, theme_config, output_dir, jinja_env, repository)
     # All GTypeInstance sub-types
     typed_tree = repository.get_class_hierarchy()
 
+    if len(objects_tree) == 0 and len(typed_tree) == 0:
+        return None
+
     res = ["<h1>Classes Hierarchy</h1>"]
 
     def dump_tree(node, out):
@@ -3116,7 +3119,9 @@ def gen_reference(config, options, repository, templates_dir, theme_config, cont
 
     content_files = gen_content_files(config, theme_config, content_dirs, ns_dir, jinja_env, namespace)
     content_images = gen_content_images(config, content_dirs, ns_dir)
-    content_files.append(gen_types_hierarchy(config, theme_config, ns_dir, jinja_env, repository))
+    types_hierarchy = gen_types_hierarchy(config, theme_config, ns_dir, jinja_env, repository)
+    if types_hierarchy:
+        content_files.append(types_hierarchy)
 
     if options.sections == [] or options.sections == ["all"]:
         gen_indices = list(all_indices.keys())
