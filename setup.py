792179822c07122222001f3575e17eea66dc3f37
Christoph Reiter <reiter.christoph@gmail.com>
2024-08-16 15:30:35 +0200

    pyproject.toml: Port to setuptools.build_meta
    If no build-backend is set then things will default to
    setuptools.build_meta:__legacy__, while the standard setuptools backend
    is setuptools.build_meta (see https://setuptools.pypa.io/en/latest/build_meta.html)
    
    The main difference is that the legacy backend adds the script directory
    to sys.path while the standard one doesn't.
    
    Our setup.py depends on this though, since it imports itself for getting
    the version. Manually add the directory to sys.path so things work as before.

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 # SPDX-FileCopyrightText: 2021 GNOME Foundation
 # SPDX-License-Identifier: Apache-2.0 OR GPL-3.0-or-later
 
+import os
 import sys
 
 if sys.version_info < (3, 6, 0):
     raise SystemExit('ERROR: GI-DocGen requires Python 3.6.0')
+
+# For the gidocgen import below. setuptools.build_meta doesn't add
+# the script dir to sys.path when building a wheel.
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
 from gidocgen.core import version
 
